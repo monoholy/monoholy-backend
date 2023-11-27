5043121dd6e82ba137a0e90f3df9ebdfe83d307a
holynekk <e238109@metu.edu.tr>
2023-11-28 01:11:26 +0300

    initial files

---
+++ src/main/java/com/monoholy/MonoholyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.monoholy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonoholyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonoholyBackendApplication.class, args);
+	}
+
+}
